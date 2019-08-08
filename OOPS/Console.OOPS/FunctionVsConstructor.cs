fcb66151b7c94ae406101d6034858c064807c2e2
anil <anili@hokuapps.com>
2019-08-09 00:26:27 +0530

    functions and constrctors

---
+++ OOPS/Console.OOPS/FunctionVsConstructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.OOPS
+{
+    /// <summary>
+    /// SUMMARY
+    /// 1. We can give any suitable name to function but constructor name will be the name of class.
+    /// 2. Function have return type but constructor don't have return type not even void.
+    /// 3. Constructor calls once at the time of object creation but funcation can be called with object and without object(static funcation) many times.
+    /// </summary>
+    public class FunctionVsConstructor
+    {
+    }
+}
