e50707dbfdad8eb7a80bc02da2ee3920d0de3987
anil <anili@hokuapps.com>
2019-08-10 02:15:06 +0530

    access specifier

---
+++ OOPS/Console.OOPS/AccessSpecifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.OOPS
+{
+    /// <summary>
+    /// SUMMARY
+    /// 1. Access specifier defines the accessibility of class member. We can restrict and limit the accessibility of member of classes.
+    /// 
+    /// ===========================================================================================================
+    /// Access Spicifier            Within class       Derived Class       Outside Class        Within Assembly
+    /// ===========================================================================================================
+    /// Public                      Yes                 Yes                 Yes                 Yes
+    /// Private                     Yes                 No                  No                  No
+    /// Protected                   Yes                 Yes                 No                  No
+    /// Internal                    Yes                 No                  No                  Yes
+    /// Protected Internal          Yes                 Yes                 No                  Yes
+    /// ===========================================================================================================
+    /// </summary>
+    class AccessSpecifier
+    {
+    }
+}
